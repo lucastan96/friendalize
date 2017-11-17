c67a18a48e2f2743ffeb8e551a463ff1aac62f9a
AlvinTeo <alvinteo9696@gmail.com>
2017-11-17 22:50:19 +0000

    done with institution

--- setup-institution-p.php
+++ setup-institution-p.php
@@ -1,5 +1,26 @@
 <?php
 
+$request_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_STRING);
 
-header("Location: get-started.php");
-exit();
+if ($request_method == 'POST') {
+    session_start();
+
+    $id = $_SESSION['user_id'];
+    $institution_id = filter_input(INPUT_POST, 'institution', FILTER_VALIDATE_INT);
+    require_once('includes/connection.php');
+
+
+    $query = "UPDATE user_institutions SET institution_id = :institution_id WHERE user_id = :id";
+    $statement = $db->prepare($query);
+    $statement->bindValue(":institution_id", $institution_id);
+    $statement->bindValue(":id", $id);
+    $statement->execute();
+    $statement->closeCursor();
+
+    header("Location: get-started.php");
+    exit();
+} else {
+    header("Location: get-started.php");
+    exit();
+}
+
