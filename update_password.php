847ef35fd3f564863fb26b4325233892aef49249
javed f <farwaj1@gmail.com>
2017-11-25 00:26:17 +0000

    updated edit

---
+++ update_password.php
@@ -0,0 +1,28 @@
+<?php
+
+$request_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_STRING);
+
+if ($request_method == 'POST') {
+    session_start();
+
+    $user_id = $_SESSION['user_id'];
+    $confirmpassword = filter_input(INPUT_POST, 'password_confirm', FILTER_SANITIZE_STRING);
+    $hashed_password = password_hash($confirmpassword, PASSWORD_DEFAULT);
+
+    require_once('includes/connection.php');
+
+    $query4 = "UPDATE users SET password = :password WHERE user_id = :user_id";
+    $statement4 = $db->prepare($query4);
+    $statement4->bindValue(":password", $hashed_password);
+    $statement4->bindValue(":user_id", $user_id);
+    $statement4->execute();
+    $statement4->closeCursor();
+    
+
+    header("Location: settings.php");
+    exit();
+} else {
+    header("Location: settings.php");
+    exit();
+}
+
