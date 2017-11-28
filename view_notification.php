05893a388cf596a4cf0bdeac103cd9f7bc30c216
javed f <farwaj1@gmail.com>
2017-11-28 20:30:17 +0000

    notifications system

---
+++ view_notification.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('includes/connection.php');
+
+$sql = "UPDATE post_comments SET status=1 WHERE status=0";
+$statement1 = $db->prepare($sql);
+$statement1->execute();
+$result = $statement1->fetchAll();
+$statement1->closeCursor();
+
+$sql2 = "select * from post_comments ORDER BY post_id DESC limit 5";
+$statement2 = $db->prepare($sql2);
+$statement2->execute();
+$result2 = $statement2->fetchAll();
+
+foreach ($result2 as $row):
+    echo $row["comment"] . "<br>";
+
+endforeach;
+
+?>
