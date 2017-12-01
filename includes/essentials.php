16d090b575145dd1029383f92b483d9e190ef6d2
javed f <farwaj1@gmail.com>
2017-12-01 20:59:36 +0000

    notifications done

--- includes/essentials.php
+++ includes/essentials.php
@@ -10,7 +10,16 @@ if ($institution_id == NULL) {
     exit();
 }
 
+$confirm_clicked = filter_input(INPUT_GET, 'clicked', FILTER_SANITIZE_STRING);
+
+if (isset($confirm_clicked)) {
+  
+    $update_comment_status = update_comment_status($db);
+    header("Location: ?");
+    exit();
+}
+
 $profile_pic = get_profile_pic($db, $_SESSION['user_id']);
 $first_name = get_first_name($db, $_SESSION['user_id']);
-$notifications_count = get_notifications_count($db);
-$result2 = get_notification_comments($db);
+$notifications_count = get_notifications_count($db, $_SESSION['user_id']);
+$result2 = get_notification_comments($db, $_SESSION['user_id']);
