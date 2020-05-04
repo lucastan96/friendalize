45234afecdf267f649fe7d07378f1e575d84db72
Lucas Tan <lucastan96@gmail.com>
2020-05-04 13:33:42 +0100

    Fix PHP array offset error

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function get_countries($db) {
+function get_countries($db)
+{
     $query = "SELECT country_id, name FROM countries";
     $statement = $db->prepare($query);
     $statement->execute();
@@ -10,7 +11,8 @@ function get_countries($db) {
     return $countries_array;
 }
 
-function get_institutions($db) {
+function get_institutions($db)
+{
     $query = "SELECT institution_id, name FROM institutions GROUP BY name";
     $statement = $db->prepare($query);
     $statement->execute();
@@ -20,7 +22,8 @@ function get_institutions($db) {
     return $institutions_array;
 }
 
-function get_interests($db) {
+function get_interests($db)
+{
     $query = "SELECT interest_id, name FROM interests WHERE interest_id != 1 GROUP BY name";
     $statement = $db->prepare($query);
     $statement->execute();
@@ -30,7 +33,8 @@ function get_interests($db) {
     return $interests_array;
 }
 
-function get_institution_id($db, $user_id) {
+function get_institution_id($db, $user_id)
+{
     $query = "SELECT institution_id FROM user_institutions WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -43,7 +47,8 @@ function get_institution_id($db, $user_id) {
     return $institution_id;
 }
 
-function get_profile_pic($db, $user_id) {
+function get_profile_pic($db, $user_id)
+{
     $query = "SELECT profile_pic FROM users WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -56,7 +61,8 @@ function get_profile_pic($db, $user_id) {
     return $profile_pic;
 }
 
-function get_first_name($db, $user_id) {
+function get_first_name($db, $user_id)
+{
     $query = "SELECT first_name FROM users WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -69,7 +75,8 @@ function get_first_name($db, $user_id) {
     return $first_name;
 }
 
-function get_user_details($db, $user_id) {
+function get_user_details($db, $user_id)
+{
     $query = "SELECT user_id,email, first_name, last_name, gender, age, country_id, DATE(join_date), profile_pic FROM users WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -80,7 +87,8 @@ function get_user_details($db, $user_id) {
     return $user_details_array;
 }
 
-function get_user_country($db, $country_id) {
+function get_user_country($db, $country_id)
+{
     $query = "SELECT name FROM countries WHERE country_id = :country_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":country_id", $country_id);
@@ -93,7 +101,8 @@ function get_user_country($db, $country_id) {
     return $country;
 }
 
-function get_user_institution($db, $user_id) {
+function get_user_institution($db, $user_id)
+{
     $query = "SELECT i.name FROM user_institutions u, institutions i WHERE u.user_id = :user_id AND u.institution_id = i.institution_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -106,7 +115,8 @@ function get_user_institution($db, $user_id) {
     return $institution;
 }
 
-function get_user_interests($db, $user_id) {
+function get_user_interests($db, $user_id)
+{
     $query1 = "SELECT interests FROM user_interests WHERE user_id = :user_id";
     $statement1 = $db->prepare($query1);
     $statement1->bindValue(":user_id", $user_id);
@@ -127,7 +137,7 @@ function get_user_interests($db, $user_id) {
         $results_array2 = $statement2->fetch();
         $statement2->closeCursor();
 
-        array_push($interests, $results_array2["name"]);
+        array_push($interests, $results_array2["name"] ?? "");
     }
 
     sort($interests);
@@ -136,7 +146,8 @@ function get_user_interests($db, $user_id) {
     return $interests_str;
 }
 
-function get_user_settings($db, $user_id) {
+function get_user_settings($db, $user_id)
+{
     $query = "SELECT first_name, last_name, gender, age, country_id FROM users WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -147,7 +158,8 @@ function get_user_settings($db, $user_id) {
     return $user_settings_array;
 }
 
-function get_user_interests_settings($db, $user_id) {
+function get_user_interests_settings($db, $user_id)
+{
     $query = "SELECT interests FROM user_interests WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -160,7 +172,8 @@ function get_user_interests_settings($db, $user_id) {
     return $interest_ids;
 }
 
-function get_rooms($db) {
+function get_rooms($db)
+{
     $query = 'SELECT * FROM ghost_room ORDER BY room_id DESC';
     $statement = $db->prepare($query);
     $statement->execute();
@@ -170,7 +183,8 @@ function get_rooms($db) {
     return $rooms;
 }
 
-function get_room_players($db, $room_id) {
+function get_room_players($db, $room_id)
+{
     $query = 'SELECT user_id FROM ghost_room_players WHERE room_id = :room_id';
     $statement = $db->prepare($query);
     $statement->execute(array(":room_id" => $room_id));
@@ -180,7 +194,8 @@ function get_room_players($db, $room_id) {
     return $users;
 }
 
-function get_room_player_info($db, $user_id) {
+function get_room_player_info($db, $user_id)
+{
     $query = "SELECT first_name, last_name, profile_pic FROM users WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -191,7 +206,8 @@ function get_room_player_info($db, $user_id) {
     return $results;
 }
 
-function check_is_joined($db, $room_id, $user_id) {
+function check_is_joined($db, $room_id, $user_id)
+{
     $query = 'SELECT COUNT(*) as is_joined FROM ghost_room_players WHERE room_id = :room_id AND user_id = :user_id';
     $statement = $db->prepare($query);
     $statement->execute(array(":room_id" => $room_id, ":user_id" => $user_id));
@@ -205,7 +221,8 @@ function check_is_joined($db, $room_id, $user_id) {
     return false;
 }
 
-function get_friend_status($db, $user_id, $friend_id) {
+function get_friend_status($db, $user_id, $friend_id)
+{
     $query1 = "SELECT friends FROM user_friends WHERE user_id = :user_id";
     $statement1 = $db->prepare($query1);
     $statement1->bindValue(":user_id", $user_id);
@@ -223,7 +240,8 @@ function get_friend_status($db, $user_id, $friend_id) {
     return false;
 }
 
-function get_friend_count($db, $user_id) {
+function get_friend_count($db, $user_id)
+{
     $query = 'SELECT friends FROM user_friends WHERE user_id = :user_id';
     $statement = $db->prepare($query);
     $statement->execute(array(":user_id" => $user_id));
@@ -240,7 +258,8 @@ function get_friend_count($db, $user_id) {
     return 0;
 }
 
-function get_friends($db, $user_id) {
+function get_friends($db, $user_id)
+{
     $query = 'SELECT friends FROM user_friends WHERE user_id = :user_id';
     $statement = $db->prepare($query);
     $statement->execute(array(":user_id" => $user_id));
@@ -257,7 +276,8 @@ function get_friends($db, $user_id) {
     return NULL;
 }
 
-function get_user_request_status($db, $user_id, $friend_id) {
+function get_user_request_status($db, $user_id, $friend_id)
+{
     $query1 = "SELECT requests FROM user_friends WHERE user_id = :user_id";
     $statement1 = $db->prepare($query1);
     $statement1->bindValue(":user_id", $user_id);
@@ -275,7 +295,8 @@ function get_user_request_status($db, $user_id, $friend_id) {
     return false;
 }
 
-function get_friend_request_status($db, $user_id, $friend_id) {
+function get_friend_request_status($db, $user_id, $friend_id)
+{
     $query1 = "SELECT requests FROM user_friends WHERE user_id = :user_id";
     $statement1 = $db->prepare($query1);
     $statement1->bindValue(":user_id", $friend_id);
@@ -293,7 +314,8 @@ function get_friend_request_status($db, $user_id, $friend_id) {
     return false;
 }
 
-function get_all_users($db) {
+function get_all_users($db)
+{
     $query = "SELECT user_id FROM users";
     $statement = $db->prepare($query);
     $statement->execute();
@@ -303,7 +325,8 @@ function get_all_users($db) {
     return $users;
 }
 
-function check_friend_requests($db, $user_id) {
+function check_friend_requests($db, $user_id)
+{
     $query = "SELECT * FROM user_friends";
     $statement = $db->prepare($query);
     $statement->execute();
@@ -312,7 +335,7 @@ function check_friend_requests($db, $user_id) {
 
     $requested_ids = [];
 
-    foreach ($results as $result):
+    foreach ($results as $result) :
         $stranger_id = $result["user_id"];
         $request_ids = $result["requests"];
         $requests_array = explode(",", $request_ids);
@@ -325,7 +348,8 @@ function check_friend_requests($db, $user_id) {
     return $requested_ids;
 }
 
-function get_id($db, $user_id) {
+function get_id($db, $user_id)
+{
     $query1 = "SELECT * FROM users WHERE user_id = :user_id";
     $statement1 = $db->prepare($query1);
     $statement1->bindValue(":user_id", $user_id);
@@ -336,7 +360,8 @@ function get_id($db, $user_id) {
     return $user;
 }
 
-function get_post_user_info($db, $user_id) {
+function get_post_user_info($db, $user_id)
+{
     $query = "SELECT first_name, last_name, profile_pic FROM users WHERE user_id = :user_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -347,7 +372,8 @@ function get_post_user_info($db, $user_id) {
     return $results;
 }
 
-function get_post_category($db, $category_id) {
+function get_post_category($db, $category_id)
+{
     $query = "SELECT name FROM interests WHERE interest_id = :interest_id";
     $statement = $db->prepare($query);
     $statement->bindValue(":interest_id", $category_id);
@@ -358,7 +384,8 @@ function get_post_category($db, $category_id) {
     return $results["name"];
 }
 
-function get_post_likes_count($db, $post_id) {
+function get_post_likes_count($db, $post_id)
+{
     $query = 'SELECT likes FROM posts WHERE post_id = :post_id';
     $statement = $db->prepare($query);
     $statement->execute(array(":post_id" => $post_id));
@@ -375,7 +402,8 @@ function get_post_likes_count($db, $post_id) {
     return 0;
 }
 
-function get_post_like_status($db, $post_id, $user_id) {
+function get_post_like_status($db, $post_id, $user_id)
+{
     $query = 'SELECT likes FROM posts WHERE post_id = :post_id';
     $statement = $db->prepare($query);
     $statement->execute(array(":post_id" => $post_id));
@@ -395,7 +423,8 @@ function get_post_like_status($db, $post_id, $user_id) {
     return 0;
 }
 
-function search_users($db, $search_query, $user_id) {
+function search_users($db, $search_query, $user_id)
+{
     $query = "SELECT user_id, first_name, last_name, profile_pic FROM users WHERE (first_name LIKE '%$search_query%' OR last_name LIKE '%$search_query%' OR username LIKE '%$search_query%' OR CONCAT_WS(' ', first_name, last_name) LIKE '%$search_query%') OR (email = '$search_query')";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -406,7 +435,8 @@ function search_users($db, $search_query, $user_id) {
     return $results;
 }
 
-function get_notifications_count($db, $user_id) {
+function get_notifications_count($db, $user_id)
+{
     $sql = 'SELECT COUNT(*) FROM post_comments WHERE status = 0 AND user_id !=:user_id';
     $res = $db->prepare($sql);
     $res->bindValue(":user_id", $user_id);
@@ -416,7 +446,8 @@ function get_notifications_count($db, $user_id) {
     return $notifications_count;
 }
 
-function get_notification_comments($db, $user_id) {
+function get_notification_comments($db, $user_id)
+{
     $query = "SELECT comment_id FROM post_comments pc, posts p WHERE pc.post_id = p.post_id AND  p.user_id != :user_id AND p.user_id = :id";
     $statement = $db->prepare($query);
     $statement->bindValue(":user_id", $user_id);
@@ -425,7 +456,8 @@ function get_notification_comments($db, $user_id) {
     $results = $statement->fetch();
     $statement->closeCursor();
 
-    if ($results['comment_id'] != $user_id) {
+    $comment_id = $results['comment_id'] ?? 0;
+    if ($comment_id != $user_id) {
         $sql2 = "SELECT * FROM post_comments WHERE user_id != $user_id ORDER BY time DESC";
         $statement2 = $db->prepare($sql2);
         $statement2->execute();
@@ -442,4 +474,3 @@ function update_comment_status($db)
     $statement1->execute();
     $statement1->closeCursor();
 }
-
